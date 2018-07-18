89a27b86740ba3f46877fe59654345ca6d52658f
Max <max.k194@gmail.com>
2018-07-18 18:28:12 +0300

    module

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var db = require('./db');
+var log = require('logger')(module);
+var db = require('db');
 db.connect();
 
 var User = require('./user');
@@ -9,7 +10,7 @@ function run() {
 
     vasya.hello(petya);
 
-    console.log(db.getPhrase("Run successful"));
+    log(db.getPhrase("Run successful"));
 }
 
 if(module.parent) {
